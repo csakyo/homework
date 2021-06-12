1a9e720bf1ad7b001be12c56b5492508162c3ed7
chihirosakyo <c.sakyou@gmail.com>
2021-06-12 11:51:43 +0900

    Improve method of display loading img

--- question07/main.js
+++ question07/main.js
@@ -1,5 +1,12 @@
 'use strict';
 
+// loading img 
+const loadImgContent = document.getElementById('loadImg');
+const loadImg = document.createElement('img');
+loadImgContent.appendChild(loadImg);
+loadImg.src = "loading-circle.gif";
+
+
 const contentsList = [{to: "bookmark.html", img: "1.png", alt:"画像1", text: "ブックマーク"}, 
                       {to: "message.html", img: "2.png", alt:"画像2", text: "メッセージ"}]
 
