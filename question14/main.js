4ed0cd538103524627d638a725944cfd4ab1e107
csakyo <c.sakyou@gmail.com>
2021-11-06 11:06:22 +0900

    add input type number in the modal

---
+++ question14/main.js
@@ -0,0 +1,79 @@
+'use strict';
+
+const wrapper = document.getElementById('js-wrapper');
+const requestBtn = document.getElementById('js-request-btn');
+const requestNumber = document.getElementById('js-number');
+const modalOpen = document.getElementById('js-modal-btn');
+const mask = document.getElementById('js-mask');
+const modal = document.getElementById('js-modal');
+const modalClose = document.getElementById('js-close');
+const inputNumber = document.getElementById("js-number");
+
+  /* MODAL OPEN */
+modalOpen.addEventListener('click',()=>{
+    modal.classList.remove('hidden');
+    mask.classList.remove('hidden');
+});
+/* MODAL close */ 
+modalClose.addEventListener('click',()=>{
+    modal.classList.add('hidden');
+    mask.classList.add('hidden');
+});
+
+mask.addEventListener('click',()=>{
+    modalClose.click();
+});
+
+async function callApi() {
+    try {
+        const res = await fetch('https://jsondata.okiba.me/v1/json/dFQLo210903231659');
+        const json = await res.json();
+        return json.data;
+    }
+    catch(error) {
+        wrapper.textContent = "データが取得できませんでした";
+        console.error(error);
+    }
+    finally{
+        modalOpen.remove();
+    }
+}
+
+async function init(){
+    const result = await callApi();
+    if (result){
+        createList(result);
+    } else {
+        wrapper.textContent = "データの表示に失敗しました";
+        console.error('データの表示に失敗しました'); 
+    }
+}
+
+requestBtn.onclick = function(){
+    if (requestNumber.value === '' || requestNumber.value === null){
+        alert('値を入力してください');
+    } else {
+        init();
+        modalClose.click();
+        console.log(inputNumber.value);
+    }
+}
+
+function createList(data){
+    const length = data.length;
+    const ul = document.getElementById('js-target');
+    const fragment = document.createDocumentFragment();
+
+    for (let i = 0; i < length; i++){
+        const {a, img, alt, text} = data[i];
+        const liTag = document.createElement('li');
+        const aTag = document.createElement('a');
+        aTag.href = a;
+        aTag.innerHTML = text;
+        const imgTag = document.createElement('img');
+        imgTag.src = img;
+        imgTag.alt = alt;
+        fragment.appendChild(liTag).appendChild(aTag).appendChild(imgTag);
+    }
+    ul.appendChild(fragment);
+}
