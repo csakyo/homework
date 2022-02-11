583f0fed25f0a743b7d25f48f6131dfe731f897a
csakyo <c.sakyou@gmail.com>
2022-02-11 15:42:50 +0900

    Refactor: Separated functions by role, modified function names, etc.

--- question16/main.js
+++ question16/main.js
@@ -8,20 +8,21 @@ const createElementWithClassName = (element, name) => {
 
 const newsWrapper = document.getElementById('js-newsWrapper');
 const tabsGroup = document.getElementById('js-tabs');
-const contentsWrapper = document.createElement("div");
+const contentsWrapper = createElementWithClassName("div", "contentsWrapper");
 contentsWrapper.id = "js-contentsWrapper";
-contentsWrapper.classList.add("contentsWrapper");
 
 
 //Get json data
 async function callApi() {
   try {
     const res = await fetch('https://myjson.dit.upm.es/api/bins/an1r');
+    if (!res.ok) {
+      throw new Error(`サーバーリクエストに失敗しました: ${res.status}`);
+    }
     const json = await res.json();
     return json.data;
   }
   catch(error) {
-    tabsGroup.textContent = "データが取得できませんでした";
     console.error(error);
   }
   finally {
@@ -30,77 +31,76 @@ async function callApi() {
 }
 
 async function init() {
-  const result = await callApi();
-  if (result){
-    renderElement(result);
+  const newsData = await callApi();
+  if (newsData){
+    renderNewsUiElement(newsData);
     } else {
-    tabsGroup.textContent = "データの表示に失敗しました";
+    newsWrapper.textContent = "データの表示に失敗しました";
     console.error('データの表示に失敗しました'); 
   }
 }
 init();
 
-//Rendering tabs and contents
-function renderElement(data){
-    newsWrapper.appendChild(contentsWrapper);
-    const length = data.length;
-    const fragmentTablists = document.createDocumentFragment();
-    const fragmentContents = document.createDocumentFragment();
-
-    //Display category names in tabs
-    for (let i = 0; i < length; i++){
-      const {category, articles, img} = data[i];
-      const tabLists = document.createElement('li');
-     
-      tabLists.innerHTML = category;
-      tabLists.classList.add('tabList');
-      fragmentTablists.appendChild(tabLists);
-      
-      const contentsContainer = createElementWithClassName("div", "contentsContainer");
-      const tabContents = createElementWithClassName("div", "tabContents");
-      const tabContentsUl = createElementWithClassName("ul", "tabContentsUl");
-
-      //Get the title of articles
-      for (let j = 0; j < articles.length; j++){
-        const articleTitle = data[i].articles[j].title;
-        const titleList = document.createElement('li');
-        const articleLink = document.createElement('a');
-        articleLink.href = "#";
-        articleLink.innerHTML = articleTitle;
-        fragmentContents.appendChild(contentsContainer).appendChild(tabContents).appendChild(tabContentsUl).appendChild(titleList).appendChild(articleLink);
-      }
-
-      //Display the img
-      const imgWrapper = createElementWithClassName("div", "imgWrapper");
-      const imgTag = document.createElement('img');
-      imgTag.src = img;
-      imgWrapper.appendChild(imgTag);
-      tabContents.appendChild(imgWrapper);
-
-      //Display the first content.
-      if (i === 0){
-        tabLists.classList.add('is-active');   
-        contentsContainer.classList.add('is-show');
-      }
-    }
-    tabsGroup.appendChild(fragmentTablists);
-    contentsWrapper.appendChild(fragmentContents);
+function renderNewsUiElement(data) {
+  tabsGroup.appendChild(createTabs(data));
+  newsWrapper.appendChild(createContents(data));
 }
 
+//Render Tab Element
+const createTabs = (data) => {
+  const fragmentTablists = document.createDocumentFragment();
+  for (let i = 0; i < data.length; i++) {
+    const tabList = createElementWithClassName("li", "tabList");
+    tabList.textContent = data[i].category;
+    i === 0 && tabList.classList.add("is-active");
+    fragmentTablists.appendChild(tabList);
+  }
+  return fragmentTablists;
+};
+
+//Render Content Element
+const createContents = (data) => {
+  const fragmentContents = document.createDocumentFragment();
+  for (let i = 0; i < data.length; i++) {
+    const {articles} = data[i];
+    const contentsContainer = createElementWithClassName("div", "contentsContainer");
+    const tabContents = createElementWithClassName("div", "tabContents");
+    const tabContentsUl = createElementWithClassName("ul", "tabContentsUl");
+    i === 0 && contentsContainer.classList.add("is-show");
+
+    for (let j = 0; j < articles.length; j++){
+      const articleTitle = data[i].articles[j].title;
+      const titleList = document.createElement('li');
+      const articleLink = document.createElement('a');
+      articleLink.href = "#";
+      articleLink.innerHTML = articleTitle;
+      fragmentContents.appendChild(contentsContainer).appendChild(tabContents).appendChild(tabContentsUl).appendChild(titleList).appendChild(articleLink);
+    }
+    tabContents.appendChild(createImgElements(data[i]));
+  }
+  contentsWrapper.appendChild(fragmentContents);
+  return contentsWrapper;
+}
+
+//get img data 
+const createImgElements = (data) => {
+  const imgWrapper = createElementWithClassName("div", "imgWrapper");
+  const imgTag = document.createElement('img');
+  imgTag.src = data.img;
+  imgWrapper.appendChild(imgTag);
+  return imgWrapper;
+}
 
 //Tab switching function
 tabsGroup.addEventListener("click", (e) => {
   const activeElement = document.getElementsByClassName("is-active")[0];
   const showElement = document.getElementsByClassName('is-show')[0];
 
-  if (activeElement && showElement && e.currentTarget !== e.target) {
-    activeElement.classList.remove("is-active");
-    e.target.classList.add("is-active");
-
-      const tabList = document.getElementsByClassName('tabList');
-      const index = Array.prototype.indexOf.call(tabList,e.target);
-      showElement.classList.remove('is-show'); 
-      document.getElementsByClassName('contentsContainer')[index].classList.add('is-show'); 
-  }
+  activeElement.classList.remove("is-active");
+  e.target.classList.add("is-active");
+  const tabList = document.getElementsByClassName('tabList');
+  const index = Array.prototype.indexOf.call(tabList,e.target);
+  showElement.classList.remove('is-show'); 
+  const contents = document.getElementsByClassName('contentsContainer');
+  contents[index].classList.add('is-show'); 
 });
-
