c445fc917be55a96082b96f65a47e654824d8a51
csakyo <c.sakyou@gmail.com>
2022-07-15 12:25:29 +0900

    feat: Creation of member registration screen

---
+++ question24/main.js
@@ -0,0 +1,45 @@
+'use strict';
+
+const wrapper = document.getElementById('js-wrapper');
+const agreementLinkBtn = document.getElementById('js-agreement-link');
+const modal = document.getElementById('js-modal');
+const modalClose = document.getElementById('js-close');
+const mask = document.getElementById('js-mask');
+const checkbox = document.getElementById('js-checkbox');
+const submitButton = document.getElementById('js-submit-button');
+const target = document.querySelector(".last_content");
+
+
+agreementLinkBtn.addEventListener('click',()=>{
+    modal.classList.remove('hidden');
+    mask.classList.remove('hidden');
+});
+modalClose.addEventListener('click',()=>{
+    modal.classList.add('hidden');
+    mask.classList.add('hidden');
+});
+mask.addEventListener('click',()=>{
+    modalClose.click();
+});
+
+const options = {
+  root: document.getElementById('js-modal'),
+  threshold: 1
+};
+
+const checkWhenIntersect = (entries) => {
+  entries.forEach(entry => {
+    if (entry.isIntersecting) {
+      checkbox.checked = true;
+      checkbox.disabled = false;
+    }
+  });
+}
+
+const observer = new IntersectionObserver(checkWhenIntersect, options);
+observer.observe(target);
+
+submitButton.addEventListener('click',function(e){
+  checkbox.checked ? location.href = './register-done.html' : e.preventDefault();
+})
+
