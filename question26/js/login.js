bcfa55551bd17d1dee3bed4264652fb2f4824212
csakyo <c.sakyou@gmail.com>
2022-09-25 16:01:54 +0900

    Feat: Create login page

---
+++ question26/js/login.js
@@ -0,0 +1,107 @@
+'use strict';
+
+const submitButton = document.getElementById('js-submit-button');
+const userInfoInputArea = document.getElementById('name_mail');
+const passwordInputArea = document.getElementById('password');
+const form = document.getElementById('form');
+
+if(localStorage.hasOwnProperty('token')) {
+  window.location.href = './newsComponents/index.html'
+}
+
+const isValidStatus = {
+  name_mail: false,
+  password: false,
+}
+
+const validationInfo = {
+  name_mail: {
+    validation: (value) => value
+  },
+  password: {
+    validation : (value) => /(?=.*[a-zA-Z])(?=.*[0-9])[a-zA-z0-9]{8,}/.test(value),
+    errorMessage: '※8文字以上の大小の英数字を交ぜたものにしてください。'  
+  }
+}
+
+const setValidationEvents = (isValid, targetForm) => {
+  if (isValid) {
+    targetForm.classList.add('valid');
+    targetForm.classList.remove('invalid');
+    targetForm.nextElementSibling.textContent = ''; 
+    isValidStatus[targetForm.id] = true;
+  } else {
+    targetForm.classList.add('invalid');
+    targetForm.classList.remove('valid');
+    targetForm.nextElementSibling.textContent = validationInfo[targetForm.id].errorMessage;
+    isValidStatus[targetForm.id] = false; 
+  }
+}
+
+const validateInputValue = (e) => {
+  const targetForm = e.target;
+  const value = targetForm.value.trim();
+  const result = validationInfo[targetForm.id].validation(value);
+  setValidationEvents(result,targetForm);
+  renderRequiredFieldMessages(targetForm);  
+  switchSubmitButton(checkAllValidity());
+  console.log(isValidStatus);
+} 
+
+const checkAllValidity = () => {
+  return Object.values(isValidStatus).every((result) => result );
+}
+
+const switchSubmitButton = (isValid) => {
+  submitButton.disabled = isValid ? false : true;
+} 
+
+const renderRequiredFieldMessages = (targetForm) => {
+  if (targetForm.value.trim() === "") {
+    targetForm.nextElementSibling.textContent = "※入力必須項目です";
+  }
+};
+
+userInfoInputArea.addEventListener('blur', validateInputValue);
+passwordInputArea.addEventListener('blur', validateInputValue);
+
+
+const checkData = (inputData) => {
+  const userData = {
+    name: "tanaka",
+    email: "tanaka@gmail.com",
+    pass: "N302aoe3"
+  }; 
+  return ((inputData.name_mail === userData.name || inputData.name_mail === userData.email) && (inputData.user_password === userData.pass)) ? true : false;
+}
+
+
+const tokenVerification = () => {
+  const userDataArr = [...new FormData(form).entries()];
+  const userData = Object.fromEntries(userDataArr);
+  return new Promise((resolve,reject) => {
+    if (checkData(userData)){
+      resolve({ token: "fafae92rfjafa03", ok: true, code: 200 });
+    } else {
+      reject({ ok: false, code: 401 }); 
+    }
+  })
+}
+
+const loginVerification = async() => {
+  try {
+    const token = await tokenVerification();
+    localStorage.setItem("token", JSON.stringify(token));
+    return true;
+  }
+  catch {
+    return false;
+  }
+}
+
+const init = async () => {
+  const verificationResult = await loginVerification();
+  verificationResult ? window.location.href = './newsComponents/index.html' : window.location.href = './loginFailure.html'
+}
+
+submitButton.addEventListener('click',init);
---
+++ question26/newsComponents/main.js
@@ -0,0 +1,155 @@
+'use strict';
+
+const createElementWithClassName = (element, name) => {
+  const createdElement = document.createElement(element);
+  createdElement.classList.add(name);
+  return createdElement;
+};
+
+const newsWrapper = document.getElementById('js-newsWrapper');
+const tabsGroup = document.getElementById('js-tabs');
+const contentsWrapper = createElementWithClassName("div", "contentsWrapper");
+contentsWrapper.id = "js-contentsWrapper";
+
+
+//Get json data
+async function callApi() {
+  try {
+    const res = await fetch('https://myjson.dit.upm.es/api/bins/5oo3');
+    if (!res.ok) {
+      throw new Error(`サーバーリクエストに失敗しました: ${res.status}`);
+    }
+    const json = await res.json();
+    return json.data;
+  }
+  catch(error) {
+    console.error(error);
+  }
+  finally {
+    console.log('CONPLETE!');
+  }
+}
+
+async function init() {
+  const newsData = await callApi();
+  if (newsData){
+    renderNewsUiElement(newsData);
+    } else {
+    newsWrapper.textContent = "データの表示に失敗しました";
+    console.error('データの表示に失敗しました'); 
+  }
+}
+init();
+
+function renderNewsUiElement(newsData) {
+  tabsGroup.appendChild(getTabListsFragment(newsData));
+  newsWrapper.appendChild(renderContents(newsData));
+}
+
+//get Tab Elements 
+const getTabListsFragment = (newsData) => {
+  const fragmentTablists = document.createDocumentFragment();
+  for (let i = 0; i < newsData.length; i++) {
+    const tabList = createElementWithClassName("li", "tabList");
+    tabList.textContent = newsData[i].category;
+    tabList.dataset.index = i;
+    newsData[i].initialDisplay && tabList.classList.add("is-active-tab");
+    fragmentTablists.appendChild(tabList);
+  }
+  return fragmentTablists;
+};
+
+//Render Content Element
+const renderContents = (newsData) => {
+  const fragmentContents = document.createDocumentFragment();
+  for (let i = 0; i < newsData.length; i++) {
+    const contentsContainer = createElementWithClassName("div", "contentsContainer");
+    const tabContents = createElementWithClassName("div", "tabContents");
+    const tabContentsUl = createElementWithClassName("ul", "tabContentsUl");
+    newsData[i].initialDisplay && contentsContainer.classList.add("is-active-content");
+
+    fragmentContents.appendChild(contentsContainer).appendChild(tabContents).appendChild(tabContentsUl).appendChild(createTitles(newsData[i]));
+    tabContents.appendChild(createImgElements(newsData[i]));
+  }
+  contentsWrapper.appendChild(fragmentContents);
+  return contentsWrapper;
+}
+
+//get titles data
+const createTitles = ({ articles }) => {
+  const fragmentTitles = document.createDocumentFragment();
+  for (let i = 0; i < articles.length; i++) {
+    const articleTitle = articles[i].title;
+    const titleList = document.createElement('li');
+    const articleLink = document.createElement('a');
+    articleLink.href = "#";
+    articleLink.textContent = articleTitle;
+
+    // Display new icon
+    const elapsedTime = getElapsedDays(articles[i].date);
+    const newArrivalPeriod = 3;
+    if (elapsedTime <= newArrivalPeriod) {
+      const newIcon = createElementWithClassName("span", "new_icon"); 
+      newIcon.textContent = 'new';
+      articleLink.appendChild(newIcon);      
+    }
+
+    //add comment icon and number
+    const commentsLength = articles[i].comments.length; 
+    if (commentsLength > 0) {
+      articleLink.appendChild(createCommentIcon(articles[i]));
+    }
+    
+    fragmentTitles.appendChild(titleList).appendChild(articleLink);
+  }
+  return fragmentTitles;
+}
+
+//Get img data 
+const createImgElements = (newsData) => {
+  const imgWrapper = createElementWithClassName("div", "imgWrapper");
+  const imgTag = document.createElement('img');
+  imgTag.src = newsData.img;
+  imgWrapper.appendChild(imgTag);
+  return imgWrapper;
+}
+
+// Display comment icons and numbers
+const createCommentIcon = (articlesData) => {
+  const commentLength = articlesData.comments.length;
+  const commentIcon = createElementWithClassName("span", "comment_icon");
+  const commentNum = createElementWithClassName("span", "comment_icon_num");
+  const commentIconImg = createElementWithClassName("img", "comment_icon_img");
+  commentNum.textContent = commentLength;
+  commentIconImg.src = './img/comment_icon.png';
+  commentIcon.appendChild(commentIconImg);
+  commentIcon.appendChild(commentNum);
+  return commentIcon; 
+}
+
+// Get the number of days elapsed
+const getElapsedDays = (postDateData) => {
+const postedDate = new Date(postDateData);
+const today = new Date();
+const millisecondsPerDay = 24*60*60*1000;
+const elapsedDays = (today - postedDate) / millisecondsPerDay;
+return Math.floor(elapsedDays);
+}
+
+//Tab switching function
+tabsGroup.addEventListener("click", (e) => {
+  const activeTab = document.getElementsByClassName("is-active-tab")[0];
+  const activeContent = document.getElementsByClassName('is-active-content')[0];
+
+  activeTab.classList.remove("is-active-tab");
+  e.target.classList.add("is-active-tab");
+  activeContent.classList.remove('is-active-content'); 
+  const contents = document.getElementsByClassName('contentsContainer');
+  contents[e.target.dataset.index].classList.add('is-active-content'); 
+});
+
+const btn = document.getElementById('js-logout-btn');
+btn.addEventListener('click', () => {
+  localStorage.removeItem('token');
+  window.location.href = '../login.html';
+});
