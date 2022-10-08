ad7c47a09e0ca7548fb3ea857e278700a03cca99
csakyo <c.sakyou@gmail.com>
2022-10-08 15:37:10 +0900

    Refactor: Delete unnecessary parts

--- question26/js/login.js
+++ question26/js/login.js
@@ -84,7 +84,7 @@ const checkData = ({ name_mail, user_password }) => {
 
 
 const userDataVerification = () => {
-  const userData = Object.fromEntries([...new FormData(form).entries()]);
+  const userData = Object.fromEntries([...new FormData(form)]);
   return new Promise((resolve,reject) => {
     if (checkData(userData)){
       resolve({ token: chance.apple_token(), ok: true, code: 200 });
