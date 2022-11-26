c53338547a3cdfae10082c6e9a5a8bc333699ed4
csakyo <c.sakyou@gmail.com>
2022-11-26 19:51:45 +0900

    Refactor: Moved to just before using variables.

--- question28/js/forgotpassword.js
+++ question28/js/forgotpassword.js
@@ -22,14 +22,14 @@ mailInputArea.addEventListener('blur', (e) => {
 const init = () => {
   const inputMailData = mailInputArea.value;
   const registeredUserData = JSON.parse(localStorage.getItem('userData'));
-  const tokenForPasswordReset = chance.apple_token();
-  const passwordResetPageUrl = "./register/password.html";
 
   if (registeredUserData === null) {
     window.location.href = './notautherize.html';
     return; 
   }
   if (registeredUserData.user_email === inputMailData) {
+    const tokenForPasswordReset = chance.apple_token();
+    const passwordResetPageUrl = "./register/password.html";
     window.location.href = `${passwordResetPageUrl}?token=${tokenForPasswordReset}`;
     return; 
   }
